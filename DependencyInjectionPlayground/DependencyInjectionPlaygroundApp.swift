477567ed9212e19920e7b1e1f69f641b1a7c80da
caokiet <kiet@cthe.dev>
2023-05-27 11:54:49 +0900

    Initial Commit

---
+++ DependencyInjectionPlayground/DependencyInjectionPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependencyInjectionPlaygroundApp.swift
+//  DependencyInjectionPlayground
+//
+//  Created by Kieran Cao on 27/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct DependencyInjectionPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
